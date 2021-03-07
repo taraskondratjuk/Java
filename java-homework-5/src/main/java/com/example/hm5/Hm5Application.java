847b4c50149f23cf52f02648693dc56b45abef18
TarasKondratiuk <taraskondratjuk@.gmail.com>
2021-03-07 22:27:54 +0200

    thymeleaf, MVC, @RequestParam, @PathVariable, @PostMapping, @GetMapping, Form

---
+++ java-homework-5/src/main/java/com/example/hm5/Hm5Application.java
@@ -0,0 +1,13 @@
+package com.example.hm5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hm5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hm5Application.class, args);
+    }
+
+}
